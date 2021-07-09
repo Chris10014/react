62f2fe63531f1372fe5c5f19d26211dd195bbfc1
Christoph Lansche <christoph.lansche@t-online.de>
2021-07-09 21:47:20 +0200

    React React Router

--- confusion/src/index.js
+++ confusion/src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap-social/bootstrap-social.css';
 import './index.css';
 import './App.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +15,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
