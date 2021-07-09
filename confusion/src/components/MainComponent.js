df6fbfe9cf990e72ff04a5841e97665c69c835d2
Christoph Lansche <christoph.lansche@t-online.de>
2021-07-09 22:46:59 +0200

    React Single Page Applications Part 1

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -2,9 +2,14 @@ import React, { Component } from "react";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Menu from "./MenuComponent";
-import Dishdetail from "./DishdetailComponent";
+// import Dishdetail from "./DishdetailComponent";
 import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+
 import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
@@ -14,14 +19,21 @@ class Main extends Component {
     
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
 
     const HomePage = () => {
-      return(
-          <Home />
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
 
@@ -31,6 +43,7 @@ class Main extends Component {
        <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
         </Switch>
         <Footer />
